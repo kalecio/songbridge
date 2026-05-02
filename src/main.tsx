5b5c399d5d71ce8dd6d186489e11e3cc08581b4c
Kalécio Pereira <kalecio.santana@gmail.com>
2026-05-02 19:53:53 -0300

    feat(ui): add React error boundary
    test(ui): add tests for the ErrorBoundary component

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router';
 import { initLogger } from './logger';
+import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary';
 
 initLogger().catch(console.error);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
